068f829a2b4a75edc4b45a6d49734b2f07aff5af
Deepwater Creations <Dylan.Craine@DeepwaterCreations.com>
2016-09-21 18:53:43 -0600

    Make a Map class

--- main.py
+++ main.py
@@ -1,58 +1,77 @@
 import random
 import math
 
-width = 50
-height = 50
+class Map(object):
+    floor = '.'
+    wall = '#'
+    impass = '~'
 
-floor = '.'
-wall = '#'
-impass = '~'
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
 
-map = []
-for y in range(height):
-    map.append([])
-    for x in range(width):
-        map[y].append(impass)
+        self._maparray = []
+
+        for y in range(self.height):
+            self._maparray.append([])
+            for x in range(self.width):
+                self._maparray[y].append(Map.impass)
+
+        self._create_map()
+
+    def get(self, x, y):
+        return self._maparray[y][x]
+
+    def set(self, x, y, tile):
+        self._maparray[y][x] = tile
+
+    def _create_map(self):
+        num_dolmens = 5
+        dolmen_radius = 6
+        for i in range(num_dolmens):
+            x = random.randint(0, self.width-1)
+            y = random.randint(0, self.height-1)
+            r = random.randint(0, dolmen_radius)
+            self.make_dolmen(x, y, r)
+        # test_dolmens()
+
+        for i_y, row in enumerate(self._maparray):
+            for i_x, tile in enumerate(row):
+                if tile == Map.floor:
+                    for neighbor in get_orthog_neighbors(i_x, i_y):
+                        if neighbor[0] < self.width and neighbor[1] < self.height and self.get(neighbor[0], neighbor[1]) == Map.impass:
+                            self.set(neighbor[0], neighbor[1], Map.wall)
+
+    def show_map(self):
+        for row in self._maparray:
+            rowstring = ""
+            for tile in row:
+                rowstring += tile
+            print rowstring
+
+    def make_dolmen(self, x, y, r):
+        for circ_height in range(-r, r+1):
+            circ_width = int(get_circle_dimensions(r, circ_height))
+            for circ_x in range(x-circ_width, x+circ_width+1):
+                if y+circ_height < self.height and circ_x < self.width:
+                    self.set(circ_x, y+circ_height, Map.floor)
+
 
 def get_circle_dimensions(r, y):
     return math.sqrt(r**2 - y**2)
 
-def make_dolmen(x, y, r):
-    for circ_height in range(-r, r+1):
-        circ_width = int(get_circle_dimensions(r, circ_height))
-        for circ_x in range(x-circ_width, x+circ_width+1):
-            if y+circ_height < height and circ_x < width:
-                map[y+circ_height][circ_x] = floor
 
 def test_dolmens():
     for r in range(0, 5):
         x = width/2
         y = r*(height/5) + 5
         make_dolmen(x, y, r)
-        map[y][x] = '@'
-
-num_dolmens = 5
-dolmen_radius = 6
-for i in range(num_dolmens):
-    x = random.randint(0, width-1)
-    y = random.randint(0, height-1)
-    r = random.randint(0, dolmen_radius)
-    print x, y, r
-    make_dolmen(x, y, r)
-# test_dolmens()
+        map.set(x, y, '@')
 
 def get_orthog_neighbors(x, y):
    return [(x+1, y), (x-1, y), (x, y+1), (x, y-1)]
 
-for i_y, row in enumerate(map):
-    for i_x, tile in enumerate(row):
-        if tile == floor:
-            for neighbor in get_orthog_neighbors(i_x, i_y):
-                if neighbor[0] < width and neighbor[1] < height and map[neighbor[1]][neighbor[0]] == impass:
-                    map[neighbor[1]][neighbor[0]] = wall
+if __name__ == "__main__":
+    map = Map(50, 50)
+    map.show_map()
 
-for row in map:
-    rowstring = ""
-    for tile in row:
-        rowstring += tile
-    print rowstring
